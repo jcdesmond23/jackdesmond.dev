057caa5c9eb32b7b3a11517a53aca538342dd7f7
jcdesmond23 <90418784+jcdesmond23@users.noreply.github.com>
2022-06-18 19:05:04 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,14 @@
 import Container from '../components/Container'
 import Head from 'next/head'
 import Hero from '../components/Hero'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from "@react-three/drei"
-
-function Box() {
-  return (
-      <mesh>
-          <boxGeometry args={[1, 1, 1]}/>
-          <meshLambertMaterial attach="material" color="black"/>
-      </mesh>
-  )
-}
 
 export default function Home() {
   return (
-      <>
-        <Canvas>
-            <OrbitControls></OrbitControls>
-            <Box></Box>
-        </Canvas>
-        <Container position='relative'>
-          <Head>
-            <title>Home - Jack Desmond</title>
-          </Head>
-          <Hero/>
-        </Container>
-      </>
+      <Container position='relative'>
+        <Head>
+          <title>Home - Jack Desmond</title>
+        </Head>
+        <Hero/>
+      </Container>
   )
 }
